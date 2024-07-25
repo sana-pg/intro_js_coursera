a933b791b9ebf39a5ac3e83a5400dc96eaa52119
sana-pg <khalil.s@pg.com>
2024-07-25 08:18:39 -0400

    added a ternary operator for authenticationStatus

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -42,3 +42,7 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
