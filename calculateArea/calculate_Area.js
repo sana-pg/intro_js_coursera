7232ce5ec45f694139eb7ee2cf53dae803dc26b1
sana-pg <khalil.s@pg.com>
2024-07-25 11:51:59 -0400

    added grocery cost calculator

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -7,3 +7,11 @@ function calculateArea() {
 let area = length * width;
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function calculateExpenditure() {
+    gro1 = parseFloat(document.getElementById('grocery1').value);
+    gro2 = parseFloat(document.getElementById('grocery2').value);
+    gro3 = parseFloat(document.getElementById('grocery3').value);
+   let cost = gro1 + gro2 + gro3;
+    document.getElementById('answer').innerText = `The grocery cost is: £ ${cost}`;
+   }
