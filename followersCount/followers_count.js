dde60998992b9b1fa913b16568d6afc71e5c46ac
sana-pg <khalil.s@pg.com>
2024-07-29 06:40:32 -0400

    reset follower count button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -15,3 +15,9 @@ function checkCountValue() {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
   }
+
+  function reset() {
+    count = 0;
+    displayCount(); // Display the count
+    alert("Follower count has reset!");
+  }
