edc50a8f2ed88d657e402e9c51ecc3a683ff33de
sana-pg <khalil.s@pg.com>
2024-07-30 11:16:29 -0400

    added search to search by specialisation

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialisation:'Javascript'},
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialisation:'Python'},
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialisation:'Java' },
       //... More employee records can be added here
     ];
 
@@ -24,6 +24,16 @@ function displayHREmployees() {
 
 function findEmployeeById(employeeId) {
     const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if (foundEmployee) {
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.specialisation}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else {
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialisation';
+    }
+}
+
+function findEmployeeBySpecialisation(employeeSpecialisation) {
+    const foundEmployee = employees.find(employee => employee.specialisation === employeeSpecialisation);
     if (foundEmployee) {
         document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     }
